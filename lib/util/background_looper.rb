ce2ff225a259f14767a6b3bff3da6ecc12a89332
Mikko Apo <mikko.apo@reaktor.fi>
2014-04-15 01:48:38 +0300

    Fixing a few bugs

--- lib/util/background_looper.rb
+++ lib/util/background_looper.rb
@@ -2,17 +2,27 @@ module Ki
   class BackgroundLooper
     attr_chain :exceptions, -> { ExceptionCatcher.new }
 
-    def run(delay=20, &block)
+    def run(delay=5, &block)
       @loop = true
       @mutex = Mutex.new
       @resource = ConditionVariable.new
       Thread.new do
+        puts "BL: new thread"
         while @loop
+          puts "LOOP"
           exceptions.catch do
-            block.call
+            puts "Block start"
+            begin
+              block.call
+            rescue Exception => e
+              puts "Exception: #{e.message} #{e.backtrace}"
+            end
+            puts "Block end"
           end
           if @loop
+            puts "before resource.wait"
             @resource.wait(@mutex, delay)
+            puts "resource.wait ended"
           end
         end
       end

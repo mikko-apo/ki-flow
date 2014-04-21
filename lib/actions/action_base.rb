fb0127836acd57ac2e7e405710722ab378a6d147
Mikko Apo <mikko.apo@reaktor.fi>
2014-04-22 01:32:50 +0300

    Fixed ä in server stdout

--- lib/actions/action_base.rb
+++ lib/actions/action_base.rb
@@ -96,7 +96,6 @@ module Ki
 
     def write_log_file(log_root)
       collect_files_from_log_dir(log_root)
-      puts log_root.inspect
       File.safe_write(action_log_file.path, JSON.pretty_generate(log_root.dup))
     end
 
